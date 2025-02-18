7393cd504c8a1b07f06c91544bb2c625247cfb27
Navid <navid@moaj.us>
2025-02-18 12:39:03 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Use JSON middleware
+app.use(express.json());
+
+// Use the router
+app.use('/', router);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
